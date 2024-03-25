08de6dacf48ade6b9bfd18c8121fbd8d654891cc
Ashwin <kasp2001@gmail.com>
2024-03-25 12:44:46 +0530

    mirage-app

--- src/index.js
+++ src/index.js
@@ -2,10 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { SnackbarProvider } from "notistack";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <SnackbarProvider
+      maxSnack={1}
+      anchorOrigin={{
+        vertical: "bottom",
+        horizontal: "center",
+      }}
+      preventDuplicate
+    >
+      <App />
+    </SnackbarProvider>
+</React.StrictMode>,
   document.getElementById('root')
 );
