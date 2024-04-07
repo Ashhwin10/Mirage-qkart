78e3b973d6adaf76f7b6491690f0cdf7e9d36b0b
Ashwin <kasp2001@gmail.com>
2024-04-07 13:53:44 +0530

    Fixed the error in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { SnackbarProvider } from "notistack";
 import { store } from "../src/redux/store.js";
 import { Provider } from "react-redux";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <SnackbarProvider
@@ -20,6 +22,5 @@ ReactDOM.render(
         <App />
       </SnackbarProvider>
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
